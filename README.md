1bec1699f42f69d10cdda653eb70a121896791a0
Nikola Rankovic <kibernick@gmail.com>
2016-06-08 23:57:37 +0200

    initial scaffold

---
+++ README.md
@@ -0,0 +1,3 @@
+# pycontacts
+
+A delightfully simple address book library in Python.
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import, print_function
+
+from setuptools import setup
+
+
+setup(name='pycontacts',
+      version='0.1',
+      description='A delightfully simple address book library in Python.',
+      url='http://github.com/kibernick/pycontacts',
+      author='Nikola Rankovic',
+      author_email='kibernick@gmail.com',
+      license='MIT',
+      packages=['pycontacts'],
+      zip_safe=False)
